5050b0e518310dda3ec6ea3da6bbe66e53c8447a
Ingeborg Eldøy <ieldoy@gmail.com>
2022-03-06 21:41:30 +0100

    curried funct

--- advance.js
+++ advance.js
@@ -1,15 +1,11 @@
 //Scope: block, function, global(nested)
-function sum(a, b, c) {
-  return a + b + c
-}
-console.log(sum(2, 3, 5))
 
 let a = 10
 function outer() {
   let b = 20
   function inner() {
     let c = 30
-    console.log(a, b, c)
+    //console.log(a, b, c)
   }
   inner()
 }
@@ -22,7 +18,7 @@ function person() {
   function adresse() {
     let husnummer = 19
     let by = 'Amsterdam'
-   console.log(`${navn} er ${alder} år og adressen er ${gate} ${husnummer} i ${by}`)
+   //console.log(`${navn} er ${alder} år og adressen er ${gate} ${husnummer} i ${by}`)
   }
   adresse()
 }
@@ -32,7 +28,7 @@ function outer() {
   let counter = 5
   function inner() {
     counter++
-    console.log(counter)
+    //console.log(counter)
   }
   return inner
 }
@@ -41,7 +37,31 @@ fn()
 fn()
 
 
+//Function currying
+//(function f(a,b,c) transforms, not call, to f(a)(b)(c))
 
+function sum(a, b, c) {
+  return a + b + c
+}
+console.log(sum(2, 3, 5))
+
+//argument hver for seg
+function curry(fn) {
+  return function(a) {
+    return function(b) {
+      return function(c) {
+        return(a,b,c)
+      }
+    }
+  }
+}
+const curriedSum = curry(sum)
+console.log(curriedSum(2)(3)(5))
+
+const add2 = curriedSum(2)
+const add3 = add2(3)
+const add5 = add3(5)
+console.log(add5)
 
 
 
