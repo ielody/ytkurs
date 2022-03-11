fc432bb6eeb9a7b5969e054b148d2bc7f30b6865
Ingeborg Eldøy <ieldoy@gmail.com>
2022-03-11 15:49:33 +0100

    Callback hell order

--- async.js
+++ async.js
@@ -14,11 +14,11 @@ clearInterval(intervalTest)
 
 //flower callback
 function pickFlower() {
-  console.log('pick flowers, and get vase')
+  //console.log('pick flowers')
 }
 
 function waterFlower() {
-  console.log('water flowers')
+  //console.log('water flowers')
 }
 
 //waterFlower(pickFlower)
@@ -27,18 +27,58 @@ const timeoutFlower = setTimeout(pickFlower, 2000)
 const timeoutVase = setTimeout(waterFlower, 2500)
 
 
-let order = (call_production) => {
-  console.log('order placed')
-  call_production()
-}
+// let order = (call_production) => {
+//   console.log('order placed')
+//   call_production()
+// }
 
-let cooking = () => {
-  console.log('order received, starting to cook')
-}
+// let cooking = () => {
+//   console.log('order received, starting to cook')
+// }
 
-order(cooking)
+// order(cooking)
 
 //storage-back-end, kitchen-front-end
 
+let stocks = {
+  Fruits: ['strawberry', 'grapes', 'watermelon'],
+  liquid: ['water', 'ice'],
+  holder: ['cone', 'cup', 'stick'],
+  toppings: ['chocolate', 'almonds']
+}
+
+let order = (Fruit_name, call_production) => {
+
+  setTimeout(() => {
+    console.log(`${stocks.Fruits[Fruit_name]} was selected`)
+    call_production()
+  }, 1000)
+
+}
+
+//Callback hell
+let cooking = () => {
+  setTimeout(() => {
+    console.log('started cutting fruit')
+
+    setTimeout(() => {
+      console.log('fruit is done')
+
+      setTimeout(() => {
+        console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was added`)
+
+        setTimeout(() => {
+          console.log(`${stocks.Fruits[0]}, ${stocks.liquid[0]} and ${stocks.liquid[1]} was added to ${stocks.holder[1]}`)
+
+          setTimeout(() => {
+            console.log(`${stocks.toppings[1]} was added to the order`)
+          }, 2000)
+        }, 1500)
+      }, 1000)
+    }, 2000)
+  },1000)
+}
+
+order(0, cooking)
 
 //Promise - create, fulfill/reject
