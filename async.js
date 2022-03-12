4952d5e4f712a7542d3fde4daa5af7374eae0984
Ingeborg Eldøy <ieldoy@gmail.com>
2022-03-12 12:00:01 +0100

    Promise

--- async.js
+++ async.js
@@ -40,6 +40,50 @@ const timeoutVase = setTimeout(waterFlower, 2500)
 
 //storage-back-end, kitchen-front-end
 
+// let stocks = {
+//   Fruits: ['strawberry', 'grapes', 'watermelon'],
+//   liquid: ['water', 'ice'],
+//   holder: ['cone', 'cup', 'stick'],
+//   toppings: ['chocolate', 'almonds']
+// }
+
+// let order = (Fruit_name, call_production) => {
+
+//   setTimeout(() => {
+//     console.log(`${stocks.Fruits[Fruit_name]} was selected`)
+//     call_production()
+//   }, 1000)
+
+// }
+
+//Callback hell
+// let cooking = () => {
+//   setTimeout(() => {
+//     console.log('started cutting fruit')
+
+//     setTimeout(() => {
+//       console.log('fruit is done')
+
+//       setTimeout(() => {
+//         console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was added`)
+
+//         setTimeout(() => {
+//           console.log(`${stocks.Fruits[0]}, ${stocks.liquid[0]} and ${stocks.liquid[1]} was added to ${stocks.holder[1]}`)
+
+//           setTimeout(() => {
+//             console.log(`${stocks.toppings[1]} was added to the order`)
+//           }, 2000)
+//         }, 1500)
+//       }, 1000)
+//     }, 2000)
+//   },1000)
+// }
+
+// order(0, cooking)
+
+
+//Promise - create, fulfill/reject
+
 let stocks = {
   Fruits: ['strawberry', 'grapes', 'watermelon'],
   liquid: ['water', 'ice'],
@@ -47,38 +91,41 @@ let stocks = {
   toppings: ['chocolate', 'almonds']
 }
 
-let order = (Fruit_name, call_production) => {
+let is_shop_open = true
 
-  setTimeout(() => {
-    console.log(`${stocks.Fruits[Fruit_name]} was selected`)
-    call_production()
-  }, 1000)
+let order = (time, work) => {
 
+  return new Promise((resolve, reject) => {
+
+    if(is_shop_open) {
+      setTimeout(()=> {
+        resolve(work())
+      }, time)
+    } else {
+      reject(console.log('our shop is closed'))
+    }
+  })
 }
 
-//Callback hell
-let cooking = () => {
-  setTimeout(() => {
-    console.log('started cutting fruit')
+order(2000, () => console.log(`${stocks.Fruits[0]} was selected`))
 
-    setTimeout(() => {
-      console.log('fruit is done')
+.then(() => {
+  return order(1000, () =>
+    console.log('Production has started'))
+})
 
-      setTimeout(() => {
-        console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} was added`)
+.then(() => {
+  return order (2000, () =>
+    console.log('fruit is done'))
+})
 
-        setTimeout(() => {
-          console.log(`${stocks.Fruits[0]}, ${stocks.liquid[0]} and ${stocks.liquid[1]} was added to ${stocks.holder[1]}`)
+.then(() => {
+  return order(1000, () =>
+    console.log(`${stocks.liquid[0]} and ${stocks.liquid[1]} were added`))
+  })
 
-          setTimeout(() => {
-            console.log(`${stocks.toppings[1]} was added to the order`)
-          }, 2000)
-        }, 1500)
-      }, 1000)
-    }, 2000)
-  },1000)
-}
-
-order(0, cooking)
-
-//Promise - create, fulfill/reject
+.then(() => {
+  return order(1000, () => {
+    console.log(`${stocks.toppings[1]} was added`)
+  })
+})
